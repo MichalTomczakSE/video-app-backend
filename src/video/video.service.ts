8d0eead07644596468f104708cbf6d766737bb15
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-09 16:23:55 +0200

    Change command for getting video url

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -27,7 +27,7 @@ export class VideoService {
       const [title, videoURL, thumbnail, duration] = await Promise.all([
         this.runCommand(`yt-dlp ${URLWithoutQueryStrings} --get-title`),
         this.runCommand(
-          `yt-dlp -f best*[vcodec!=none][acodec!=none] ${URLWithoutQueryStrings} -g`,
+          `yt-dlp -f -b ${URLWithoutQueryStrings} -g`,
         ),
         this.runCommand(`yt-dlp ${URLWithoutQueryStrings} --get-thumbnail`),
         this.runCommand(`yt-dlp ${URLWithoutQueryStrings} --get-duration`),
