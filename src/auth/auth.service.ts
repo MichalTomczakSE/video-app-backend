090b9ccdbcf6588307fbef63b1b32a772ce91d73
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-11 13:14:41 +0200

    Add username to login method return.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
   async login(user: User) {
     const payload = { username: user.username, sub: user.userId };
     return {
+      name: user.name,
       access_token: this.jwtService.sign(payload),
     };
   }
