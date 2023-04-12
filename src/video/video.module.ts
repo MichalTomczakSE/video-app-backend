508182f966839651af8eda0796c0a7927b8f3169
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-12 19:43:52 +0200

    Add dependencies to VideoService Module

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -1,4 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoEntity } from './entities/video.entity';
 import { Module } from '@nestjs/common';
+import { VideoService } from './video.service';
+import { VideoController } from './video.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([VideoEntity])],
+  controllers: [VideoController],
+  providers: [VideoService],
+  exports: [VideoService],
+})
 export class VideoModule {}
