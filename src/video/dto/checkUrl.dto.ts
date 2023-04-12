b453b946f53bac3c6143788c6e745d0ec62e6449
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-12 19:45:25 +0200

    Create video repository entity

--- src/video/dto/checkUrl.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CheckUrlDto {
-  videoAddress: URL;
-}
---
+++ src/video/entities/video.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Entity()
+export class VideoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  videoUrl: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  thumbnailUrl: string;
+  @Column()
+  duration: string;
+
+  @ManyToOne(() => User, (user) => user.videos)
+  user: User;
+}
