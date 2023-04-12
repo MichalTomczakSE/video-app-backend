bd18a047dc620f5369bd6c373b1d9acf4ad40e3c
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-12 19:19:11 +0200

    Refactor director structure

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   async validate(payload: any) {
-    return await this.usersService.findOne(payload.sub);
+    return { userId: payload.sub, username: payload.username };
   }
 }
